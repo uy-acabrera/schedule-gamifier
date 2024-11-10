87495a4e674118fab4d8bda19333406267c74390
Alex Cabrera <uy.acabrera@gmail.com>
2024-11-10 00:33:22 -0300

    add default activities

--- src/app/services/activity.service.ts
+++ src/app/services/activity.service.ts
@@ -5,65 +5,120 @@ import { Activity } from '../interfaces/activity.interface';
 import { MoodToActivityMapping } from '../interfaces/mood-to-activity-mapping';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ActivityService {
   private activities: Activity[] = [
-    // { type: ActivityTpe.mental, description: 'Solve a Riddle: What has keys but no locks, space but no room, and you can enter but not go in?' },
-    // { type: ActivityTpe.mental, description: 'Do a Quick Math: Calculate 15% of 340 without a calculator' },
-    // { type: ActivityTpe.mental, description: 'Memory Challenge: Memorize a sequence of 10 random numbers' },
-    // { type: ActivityTpe.creative, description: 'Draw your favorite animal in a superhero costume' },
-    // { type: ActivityTpe.creative, description: 'Write a haiku about your current mood' },
-    // { type: ActivityTpe.creative, description: 'Design a logo for your imaginary company' },
-    // { type: ActivityTpe.physical, description: 'Do 20 jumping jacks' },
-    // { type: ActivityTpe.physical, description: 'Hold a plank position for 30 seconds' },
-    // { type: ActivityTpe.physical, description: 'Dance to your favorite song' },
-    // { type: ActivityTpe.relaxing, description: 'Take 10 deep breaths, counting each one' },
-    // { type: ActivityTpe.relaxing, description: 'Close your eyes and imagine your happy place for 1 minute' },
-    // { type: ActivityTpe.relaxing, description: 'Stretch your arms and legs for 2 minutes' }
+    {
+      type: ActivityType.Mental,
+      time: 5,
+      description:
+        "Solve a Riddle: What has keys but no locks, space but no room, and you can enter but not go in?",
+    },
+    {
+      type: ActivityType.Mental,
+      time: 10,
+      description: "Do a Quick Math: Calculate 15% of 340 without a calculator",
+    },
+    {
+      type: ActivityType.Mental,
+      time: 15,
+      description: "Memory Challenge: Memorize a sequence of 10 random numbers",
+    },
+    {
+      type: ActivityType.Creative,
+      time: 5,
+      description: "Draw your favorite animal in a superhero costume",
+    },
+    {
+      type: ActivityType.Creative,
+      time: 10,
+      description: "Write a haiku about your current mood",
+    },
+    {
+      type: ActivityType.Creative,
+      time: 15,
+      description: "Design a logo for your imaginary company",
+    },
+    { 
+      type: ActivityType.Physical,
+      time: 5,
+      description: "Do 20 jumping jacks"
+    },
+    {
+      type: ActivityType.Physical,
+      time: 30,
+      description: "Hold a plank position for 30 seconds",
+    },
+    { 
+      type: ActivityType.Physical,
+      time: 60,
+      description: "Dance to your favorite song"
+    },
+    {
+      type: ActivityType.Relaxing,
+      time: 30,
+      description: "Play magic for 30 seconds",
+    },
+    {
+      type: ActivityType.Relaxing,
+      time: 10,
+      description: "Close your eyes and imagine your happy place for 10 seconds",
+    },
+    {
+      type: ActivityType.Relaxing,
+      time: 60,
+      description: "Stretch your arms and legs for 1 minute",
+    },
   ];
 
   constructor() {
-    this.activities = this.getActivitiesFromStorage();
+    this.activities.push(...this.getActivitiesFromStorage());
   }
 
   getActivitiesFromStorage(): Activity[] {
-    const activitiesJSON = localStorage.getItem('activities');
+    const activitiesJSON = localStorage.getItem("activities");
     if (!activitiesJSON) {
       return [];
     }
-  
+
     try {
       const activities: Activity[] = JSON.parse(activitiesJSON);
       return activities;
     } catch (error) {
-      console.error('Error parsing activities from local storage:', error);
+      console.error("Error parsing activities from local storage:", error);
       return [];
     }
   }
 
-  private getRecommendedActivities(mood: Mood, activities: Activity[]): Activity[] {
+  private getRecommendedActivities(
+    mood: Mood,
+    activities: Activity[]
+  ): Activity[] {
     const { types, maxTime } = MoodToActivityMapping[mood];
-    return activities.filter(activity => 
-      types.includes(activity.type) && 
-      activity.time <= maxTime
+    return activities.filter(
+      (activity) => types.includes(activity.type) && activity.time <= maxTime
     );
   }
 
   getRandomActivity(type?: ActivityType, mood?: Mood): Activity {
-    const filteredActivitiesByMood = mood ? this.getRecommendedActivities(mood, this.activities) : this.activities;
+    const filteredActivitiesByMood = mood
+      ? this.getRecommendedActivities(mood, this.activities)
+      : this.activities;
 
-    const filteredActivitiesByType = type 
-      ? filteredActivitiesByMood.filter(activity => activity.type === type)
+    const filteredActivitiesByType = type
+      ? filteredActivitiesByMood.filter((activity) => activity.type === type)
       : filteredActivitiesByMood;
-    
-    return filteredActivitiesByType[Math.floor(Math.random() * filteredActivitiesByType.length)];
+
+    return filteredActivitiesByType[
+      Math.floor(Math.random() * filteredActivitiesByType.length)
+    ];
   }
 
   saveActivity(activity: Activity) {
     this.activities.push(activity);
     const activitiesJSON = JSON.stringify(this.activities);
-    localStorage.setItem('activities', activitiesJSON);
+    localStorage.setItem("activities", activitiesJSON);
   }
 }
 
